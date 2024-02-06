9ee0e08bbd8d330d689b02341baf59e3cd0a9d3a
Prokopchuk-Valentin <valentin.p@rocketfirm.tech>
2024-02-06 13:04:06 +0600

    добавляет пути

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import circleDependency from 'vite-plugin-circular-dependency';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    circleDependency({
+      include: /src\/.*\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      circleImportThrowErr: true,
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      '@hooks': resolve(__dirname, 'src/hooks'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@api': resolve(__dirname, 'src/api'),
+      '@context': resolve(__dirname, 'src/context'),
+      '@types': resolve(__dirname, 'src/types'),
+      '@utils': resolve(__dirname, 'src/utils'),
+    },
+  },
+});
