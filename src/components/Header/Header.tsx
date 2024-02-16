ec60d9431f812626a1621d99a56e40da0d547022
Prokopchuk-Valentin <valentin.p@rocketfirm.tech>
2024-02-17 02:12:00 +0600

    дорабатывает header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,29 @@
-import { themeConstants } from '@constants/themeConstants';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+
 import { useTheme } from '@hooks/useTheme';
-import { Outlet } from 'react-router-dom';
+
+import { login, logout } from '@context/auth';
+import { themeConstants } from '@constants/themeConstants';
+import { navigateApiChanged } from '@context/navigate';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const userName = login.useUserName();
+  const isAuth = login.useIsAuth();
+
   const { swithTheme, theme } = useTheme();
+
   const { initialTheme } = themeConstants;
+
+  useEffect(() => {
+    navigateApiChanged(navigate);
+  }, []);
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <>
       <header
@@ -14,14 +33,28 @@ export default function Header() {
       >
         <div className="container">
           <h1 style={{ color: 'white' }}>Мои расходы</h1>
-          <button
-            className={`btn btn-${
-              theme === initialTheme ? 'light' : initialTheme
-            }`}
-            onClick={swithTheme}
-          >
-            Переключить на {initialTheme === theme ? 'светлую' : 'тёмную'} тему
-          </button>
+          {isAuth && <h2 style={{ color: 'white' }}>{userName}</h2>}
+          <div style={{ display: 'flex', gap: 10 }}>
+            <button
+              className={`btn btn-${
+                theme === initialTheme ? 'light' : initialTheme
+              }`}
+              onClick={swithTheme}
+            >
+              Переключить на {initialTheme === theme ? 'светлую' : 'тёмную'}{' '}
+              тему
+            </button>
+            {isAuth && (
+              <button
+                className={`btn btn-${
+                  theme === initialTheme ? 'light' : initialTheme
+                }`}
+                onClick={handleLogout}
+              >
+                Выйти
+              </button>
+            )}
+          </div>
         </div>
       </header>
       <Outlet />
