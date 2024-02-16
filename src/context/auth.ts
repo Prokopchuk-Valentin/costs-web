1765be3eb10836c1059446516dabba19580e4710
Prokopchuk-Valentin <valentin.p@rocketfirm.tech>
2024-02-17 02:12:42 +0600

    дорабатывает авторизацию, переносит логику на effects

--- src/context/auth.ts
+++ src/context/auth.ts
@@ -1,9 +1,118 @@
-import { createDomain } from 'effector';
+import { api } from '@api/axiosClient';
+import persist from 'effector-localstorage';
+import { AxiosError, AxiosResponse } from 'axios';
 
-const auth = createDomain();
+import { createEffect, createEvent, createStore, sample } from 'effector';
+import { useUnit } from 'effector-react';
+import { navigateFx } from './navigate';
 
-const setAuth = auth.createEvent<boolean>();
+interface AuthResponse {
+  accessToken: string;
+  refreshToken: string;
+  userName: string;
+}
 
-export const $auth = auth
-  .createStore<boolean>(false)
-  .on(setAuth, (_, value) => value);
+interface RegitrationResponse {
+  message: string;
+}
+
+interface RequestLogReg {
+  userName: string;
+  password: string;
+}
+
+export const logout = createEvent();
+export const setUserName = createEvent<string>();
+export const logInEvent = createEvent<RequestLogReg>();
+export const registerEvent = createEvent<RequestLogReg>();
+
+sample({
+  clock: logout,
+  fn: () => ({ path: '/login' }),
+  target: navigateFx,
+});
+
+export const logInFx = createEffect<
+  RequestLogReg,
+  AxiosResponse<AuthResponse>,
+  AxiosError<{ message: string }>
+>('login', {
+  handler: async ({ userName, password }) =>
+    await api.post<AuthResponse>('/auth/login', {
+      userName,
+      password,
+    }),
+});
+
+sample({
+  clock: logInEvent,
+  target: logInFx,
+});
+
+sample({
+  clock: logInFx.doneData,
+  filter: (response: AxiosResponse<AuthResponse>) => response.status === 200,
+  fn: () => ({ path: '/costs' }),
+  target: navigateFx,
+});
+
+export const registerFx = createEffect<
+  RequestLogReg,
+  AxiosResponse<RegitrationResponse>,
+  AxiosError<{ message: string }>
+>('login', {
+  handler: async ({ userName, password }) =>
+    await api.post<RegitrationResponse>('/auth/registration', {
+      userName,
+      password,
+    }),
+});
+
+sample({
+  clock: registerEvent,
+  target: registerFx,
+});
+
+sample({
+  clock: registerFx.doneData,
+  fn: () => ({ path: '/login' }),
+  target: navigateFx,
+});
+
+export const $loginStoreResponse = createStore<AuthResponse | object>({}).on(
+  logout,
+  () => {}
+);
+
+sample({
+  clock: logInFx.doneData,
+  filter: (response: AxiosResponse<AuthResponse>) => response.status === 200,
+  fn: (response) => response.data,
+  target: $loginStoreResponse,
+});
+
+persist({
+  store: $loginStoreResponse,
+  key: 'authData',
+});
+
+export const $isAuth = $loginStoreResponse.map(
+  (state) =>
+    'accessToken' in state && 'refreshToken' in state && 'userName' in state
+);
+
+export const $isLoading = createStore<boolean>(false)
+  .on(logInFx.pending, (_, pending) => pending)
+  .on(registerFx.pending, (_, pending) => pending);
+
+export const $userName = $loginStoreResponse.map(
+  (state: AuthResponse | object) =>
+    state && 'userName' in state ? state?.userName : ''
+);
+
+export const login = {
+  useUserName: () => useUnit($userName),
+  useIsAuth: () => useUnit($isAuth),
+  useLoginStoreResponse: () => useUnit($loginStoreResponse),
+  useIsLoadingLogin: () => useUnit($isLoading),
+};
